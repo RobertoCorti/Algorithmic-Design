12db3ba486321c2a626fd82ee444e50b3941022f
Roberto Corti <robi290596@gmail.com>
2020-05-03 17:24:09 +0200

    homework04: ex01

---
+++ homeworks/homework_4/src/bubble_sort.c
@@ -0,0 +1,18 @@
+#include "bubble_sort.h"
+
+#define ADDR(A, i, key_size) (A+(i)*(key_size))
+
+void bubble_sort(void *A, const unsigned int n, 
+                 const size_t elem_size, 
+                 total_order leq)
+{
+
+	for(unsigned int i=n; i>0; i--){
+		for(unsigned int j=0; j<i; j++){
+			if( !(leq(ADDR(A,j,elem_size), ADDR(A,j+1,elem_size))) ){
+				swap( ADDR(A,j,elem_size), ADDR(A,j+1,elem_size), elem_size );
+			}	
+		}
+	}
+    
+}
