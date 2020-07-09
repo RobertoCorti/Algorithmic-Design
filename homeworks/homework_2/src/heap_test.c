f2a3185034f190c03103e846168b26fee1893d5d
Roberto Corti <robi290596@gmail.com>
2020-07-09 15:47:36 +0200

    homework_02: forgot to add src/heap_test.c

---
+++ homeworks/homework_2/src/heap_test.c
@@ -0,0 +1,51 @@
+#include "binheap.h"
+#include "total_orders.h"
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+
+#define MAX_SIZE 15
+
+void int_printer(const void *value)
+{
+  printf("%d", *((const int *)value));
+}
+
+
+int main(){
+
+	int *B=(int *)malloc(sizeof(int)*MAX_SIZE);
+  	
+  	for(int k=0; k<MAX_SIZE; k++){
+  		B[k] = (rand() % (MAX_SIZE + 1)); 
+  	}
+
+	printf("Call build_heap\n");
+	binheap_type *minH= build_heap(B,MAX_SIZE-2,MAX_SIZE,sizeof(int),leq_int);
+	print_heap(minH, int_printer);
+	
+	printf("Call heapify on %d \n", B[MAX_SIZE/2]);
+	heapify(minH, B[0]);
+	print_heap(minH, int_printer);
+	
+	printf("Call is_heap_empty  (False=0) (True=1) \n");
+	printf("Output: %d \n", is_heap_empty(minH));
+	
+	printf("Call min_value: \n");
+	int s = *( (int*) min_value(minH) );
+	printf("Output: %d \n", s );
+	
+	
+	printf("Call insert value = 0\n");
+	int i=0;
+	insert_value(minH, &i);
+	print_heap(minH, int_printer);	
+
+	printf("Call delete_heap\n");
+	delete_heap(minH);
+
+	return 0;
+}
